675a781bd6a0fec009b788c5ebf25681bf60ea10
Juliana099 <146016662+Sofiaj099@users.noreply.github.com>
2025-04-04 19:46:35 +0000

    holaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
@@ -12,22 +12,23 @@ import Usuarios from './componentes/usuarios'
 import Pokemon from './componentes/pokemon'
 import Aleatorios from './componentes/aleatorios';
 import Capturados from './componentes/capturados';
-
+import Menu from './componentes/Menu';
 
 function App() {
 
 
   return (
-    <router>
+    <Router>
+      <Menu/>
       <Routes>
-      <router path= "/" element={<Listas/>}/>
-      <router path= "/Favoritos" element={<Favoritos/>}/>
-      <router path= "/Aleatorios" element={<Aleatorios/>}/>
-      <router path= "/Capturados" element={<Capturados/>}/>
-      <router path= "/Usuarios" element={<Usuarios/>}/>
-      <router path= "/Pokemon" element={<Pokemon/>}/>
+      <Route path= "/" element={<Listas/>}/>
+      <Route path= "/Favoritos" element={<Favoritos/>}/>
+      <Route path= "/Aleatorios" element={<Aleatorios/>}/>
+      <Route path= "/Capturados" element={<Capturados/>}/>
+      <Route path= "/Usuarios" element={<Usuarios/>}/>
+      <Route path= "/Pokemon" element={<Pokemon/>}/>
       </Routes>
-    </router>
+    </Router>
   )
 }
 
