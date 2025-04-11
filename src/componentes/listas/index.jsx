010d79528289aac8a5a82368432591a85491d44e
Juliana099 <146016662+Sofiaj099@users.noreply.github.com>
2025-04-11 18:22:11 +0000

    no

--- src/componentes/listas/index.jsx
+++ src/componentes/listas/index.jsx
@@ -1,33 +1,42 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-import './style.css'
+import './style.css';
+import Filtro from '../filtro';
 
 function Listas() {
-
   const [data, setData] = useState([]);
+
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=1025")
       .then(response => response.json())
       .then(responseData => setData(responseData.results))
       .catch(error => console.error("Error:", error));
-  }, []); 
+  }, []);
 
+  const handleTipoChange = (tipo) => {
+    console.log("Tipo seleccionado:", tipo);
+    // Puedes hacer lógica con el filtro aquí
+  };
 
   return (
-
-    <section className='c-lista'>
-    {data.map((pokemon, index) => (
-      <div className='c-lista-pokemon'
-      key={index}>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`} 
-              alt={`Pokémon ${pokemon.name}`} width='auto' height='60' loading='lazy'
+    <>
+      <Filtro onTipoChange={handleTipoChange} />
+      <section className='c-lista'>
+        {data.map((pokemon) => (
+          <div className='c-lista-pokemon' key={pokemon.name}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`}
+              alt={`Pokémon ${pokemon.name}`}
+              width='auto'
+              height='60'
+              loading='lazy'
             />
-        <p>{pokemon.name}</p>
-      </div>
-    ))}
-  </section>
-);
-
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
 
-export default Listas
+export default Listas;
